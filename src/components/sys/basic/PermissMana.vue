bc039c23f336a395f3201607572d1bba6c282184
git <2415964836@qq.com>
2020-03-18 22:41:44 +0800

    20200318 66

--- src/components/sys/basic/PermissMana.vue
+++ src/components/sys/basic/PermissMana.vue
@@ -1,13 +1,75 @@
 <template>
-    <div>权限管理</div>
+    <div>
+        <div class="permissionManeTool">
+        <el-input  size="small" placeholder="请输入英文角色名称" v-model="role.name">
+            <template slot="prepend">ROLE_</template>
+        </el-input>
+        <el-input  size="small" placeholder="请输入中文角色名称" v-model="role.nameZh">
+        </el-input>
+            <el-button size="small"  type="primary" icon="el-icon-plus">添加</el-button>
+        </div>
+        <div class="permissManaMain">
+            <el-collapse v-model="activeName" accordion>
+                <el-collapse-item :title="r.nameZh" :name="r.id" v-for="(r,index) in roles" :key="index">
+                    <el-card class="box-card">
+                        <div slot="header" class="clearfix">
+                            <span>可访问的资源</span>
+                            <el-button style="float: right; padding: 3px 0;color: #ff315d" icon="el-icon-delete" type="text"></el-button>
+                        </div>
+                        <div v-for="o in 4" :key="o" class="text item">
+                            {{'列表内容 ' + o }}
+                        </div>
+                    </el-card>
+                </el-collapse-item>
+
+            </el-collapse>
+        </div>
+    </div>
+
 </template>
 
 <script>
     export default {
-        name: "PermissMana"
+        name: "PermissMana",
+        data(){
+            return{
+                activeName:2,
+                role:{
+                    name:'',
+                    nameZh:''
+                },
+                roles:[]
+            }
+        },
+        mounted() {
+            this.initRoles();
+        },
+        methods:{
+initRoles(){
+    this.getRequest("/system/basic/permiss/").then(resp=>{
+        if (resp) {
+            this.roles=resp;
+        }
+    })
+}
+
+      }
+
     }
 </script>
 
 <style scoped>
+.permissionManeTool{
+display: flex;
+    justify-content: flex-start;
 
+}
+    .permissionManeTool .el-input{
+        width: 300px;
+        margin-right: 6px;
+    }
+    .permissManaMain{
+        margin-top: 10px;
+        width: 700px;
+    }
 </style>
--- src/components/sys/basic/PosMana.vue
+++ src/components/sys/basic/PosMana.vue
@@ -41,6 +41,14 @@
                         width="150"
                         label="创建时间">
                 </el-table-column>
+                <el-table-column
+                        label="是否启用">
+                    <template slot-scope="scope">
+                        <el-tag size="small" type="success" v-if="scope.row.enabled">已启用</el-tag>
+                        <el-tag size="small" type="danger"  v-else>未启用</el-tag>
+
+                    </template>
+                </el-table-column>
                 <el-table-column label="操作">
 
                     <template slot-scope="scope">
@@ -68,11 +76,23 @@
                 :visible.sync="dialogVisible"
                 width="30%">
 
-            <div>
-                <el-tag>职位名称</el-tag>
-                <el-input class="updatePosInput"
-                v-model="updatePos.name"></el-input>
-            </div>
+       <div>
+           <div>
+
+                   <el-tag>职位名称</el-tag>
+                   <el-input class="updatePosInput"
+                             v-model="updatePos.name"></el-input>
+
+           </div>
+           <div>
+               <el-tag> 是否启用</el-tag>
+               <el-switch
+                       v-model="updatePos.enabled"
+                       active-text="启用"
+                       inactive-text="禁用">
+               </el-switch>
+           </div>
+       </div>
             <span slot="footer" class="dialog-footer">
     <el-button size="small" @click="dialogVisible = false">取 消</el-button>
     <el-button size="small" type="primary" @click="doUpdate">确 定</el-button>
@@ -91,7 +111,8 @@
                 },
                 dialogVisible:false,
                 updatePos:{
-                  name:''
+                  name:'',
+                    enabled:false
                 },
 
                 multipleSelection: [],
